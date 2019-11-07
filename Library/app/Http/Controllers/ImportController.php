028e6a2746be8e6cd2684957bd4ef3600376550a
Will McGill <mcgill.will@yahoo.com>
2019-11-07 15:33:31 -0500

    added api import

--- Library/app/Http/Controllers/ImportController.php
+++ Library/app/Http/Controllers/ImportController.php
@@ -16,14 +16,32 @@ class ImportController extends Controller
         $result = $client->get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyByTmfRS8z0kdW8_vlD2-bRkAzX4XJU3Ww');
       
         $books = json_decode($result->getBody()->getContents())->items;
-        //dd($books->volumeInfo->title);
+        //dd($books);
 
         foreach($books as $newBook){
-            //echo($book->volumeInfo->title);
+            
+           
         $book = new Book();
         $book->title = $newBook->volumeInfo->title;
-        $book->author = $newBook->volumeInfo->authors;
-        $book->genre = $newBook->volumeInfo->categories;
+
+        if ($newBook->volumeInfo->authors[0] == null){
+
+            $book->author = "unknown";
+        }
+        else 
+        {
+            $book->author = $newBook->volumeInfo->authors[0];
+        };
+
+        if (isset($newBook->volumeInfo->categories[0])){
+
+        $book->genre = $book->genre = $newBook->volumeInfo->categories[0];
+        }
+        else
+        {
+            $book->genre = 'unknown';
+        }
+        
         
         $book->save();
         }
